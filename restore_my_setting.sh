23ac89ffbd4db898095544e936c21eabe231d04a
bill.2714 <bill.2714@kakaocorp.com>
2017-01-03 11:00:17 +0900

    맥일경우만 brew 설치
    주석 제거

--- restore_my_setting.sh
+++ restore_my_setting.sh
@@ -8,31 +8,26 @@ git config --global alias.co checkout
 git config --global alias.ci commit
 git config --global alias.st status
 
-# install common programs
 sh ./installcommon.sh
 #chsh -s /usr/local/bin/zsh
 zsh ./installprezto.zsh
 
-# restore brew, pip programs
-sh ./installbybrew.sh
+if [[ $(uname) == 'Darwin' ]]; then
+    sh ./installbybrew.sh
+fi
+
 sh ./installbypip.sh
 
-# restore shell settings
 cp -fv ./.bashrc ~/.bashrc
 cp -fv ./.zshrc ~/.zshrc
-
-# backup vimrc
 cp -fv ./.vimrc ~/.vimrc
 
-# restore .ssh settings
 # 보안사항으로 실제 필요할때만 사용하자
 # cp -fv ./.ssh/* ~/.ssh
 
-# restore hosts settings
 # 보안사항으로 실제 필요할때만 사용하자
 #sudo cp -fv hosts /etc/hosts
 
-# install vimplugin
 sh ./installvimplugin.sh
 
 if [[ $(uname) == 'Darwin' ]]; then
@@ -40,7 +35,6 @@ if [[ $(uname) == 'Darwin' ]]; then
     if [ -e '/Applications/iTerm.app' ]; then
         echo "file exists /Applications/iTerm.app"
     else
-        # download and install iterm2
         wget https://iterm2.com/downloads/stable/iTerm2-3_0_12.zip
         tar zxvf iTerm2-3_0_12.zip
         sudo cp -Rv "iTerm.app" /Applications
@@ -50,7 +44,6 @@ if [[ $(uname) == 'Darwin' ]]; then
     if [ -e '/Applications/SourceTree.app' ]; then
         echo "file exists /Applications/SourceTree.app"
     else
-        # download and install sourcetree
         wget https://downloads.atlassian.com/software/sourcetree/SourceTree_2.4c.zip
         tar zxvf SourceTree_2.4c.zip
         sudo cp -Rv "SourceTree.app" /Applications
@@ -60,14 +53,11 @@ if [[ $(uname) == 'Darwin' ]]; then
     if [ -e '/Applications/Visual Studio Code.app' ]; then
         echo "file exists /Applications/Visual Studio Code.app"
     else
-        # download and install vscode
         wget https://az764295.vo.msecnd.net/stable/7ba55c5860b152d999dda59393ca3ebeb1b5c85f/VSCode-darwin-stable.zip
         tar zxvf VSCode-darwin-stable.zip
         sudo cp -Rv "Visual Studio Code.app" /Applications
         rm -rfv "Visual Studio Code.app" "VSCode-darwin-stable.zip"
-        # restore vscode settings
         cp -fv ./.vscode/*.json ~/Library/Application\ Support/Code/User/ 
-        # restore vscode extension
         sh ./install_vscode_extension.sh
     fi
 
@@ -75,7 +65,6 @@ if [[ $(uname) == 'Darwin' ]]; then
     if [ -e '/Applications/Slack.app' ]; then
         echo "file exists /Applications/Slack.app"
     else
-        # download and install slack
         wget https://downloads.slack-edge.com/mac_releases/Slack-2.3.3-macOS.zip
         tar zxvf Slack-2.3.3-macOS.zip
         sudo cp -Rv ".app" /Applications
@@ -85,7 +74,6 @@ if [[ $(uname) == 'Darwin' ]]; then
     if [ -e '/Applications/Google Chrome.app' ]; then
         echo "file exists /Applications/Google Chrome.app"
     else
-        # download and install chrome
         wget https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg
         hdiutil mount googlechrome.dmg
         sudo cp -Rv "/Volumes/Google Chrome/Google Chrome.app" /Applications
