8c61f0ed4ee79575cd0363b65d0ef2c84a30573d
bill.2714 <bill.2714@kakaocorp.com>
2017-01-11 10:31:17 +0900

    update vscode settings and extension

--- install_vscode_extension.sh
+++ install_vscode_extension.sh
@@ -19,6 +19,7 @@ code --install-extension mrmlnc.vscode-apache
 code --install-extension ms-vscode.Theme-MaterialKit
 code --install-extension ms-vscode.cpptools
 code --install-extension ms-vscode.csharp
+code --install-extension qinjia.view-in-browser
 code --install-extension ravilang.ravi-debug
 code --install-extension robertohuertasm.vscode-icons
 code --install-extension timonwong.ansible-autocomplete
