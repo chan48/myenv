8d85843354db3fc63a97429d21ab8b0190b96528
bill.2714 <bill.2714@kakaocorp.com>
2017-03-08 11:43:21 +0900

    git 초기 설정 추가

--- restore_my_setting.sh
+++ restore_my_setting.sh
@@ -2,11 +2,15 @@
 # ysoftman
 # restore my settings
 
-# add git alias
+# git config
 git config --global alias.br branch
 git config --global alias.co checkout
 git config --global alias.ci commit
 git config --global alias.st status
+git config --global color.ui auto
+git config --global core.editor vim
+git config --global merge.tool vimdiff
+
 
 sh ./installcommon.sh
 
